1599e18e9670f6b607729dc62c2d98ae0f1ab529
LZRS1 <153413019+LZRS1@users.noreply.github.com>
2024-11-11 08:06:09 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,22 @@ const nextConfig = {
 			{
 				protocol: "https",
 				hostname: "https://l4zarus.site",
+			},
+			{
+				protocol: "https",
+				hostname: "https://l4zarus.space",
+			},
+			{
+				protocol: "https",
+				hostname: "https://l4zarus-lzrs2s-projects.vercel.app",
+			}
+			{
+				protocol: "https",
+				hostname: "https://l4zarus-git-main-lzrs2s-projects.vercel.app",
+			},
+			{
+				protocol: "https",
+				hostname: "https://l4zarus-g0j1nwwqc-lzrs2s-projects.vercel.app",
 			}
 		],
 	},
